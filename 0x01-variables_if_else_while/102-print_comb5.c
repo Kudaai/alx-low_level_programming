f10e9b251b69e1b6c8150a6a8f51014b2de2d060
lking <abdelaliell14@gmail.com>
2023-03-17 08:42:48 -0700

    second commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
+
 /**
- * main - prints all numbers of base 16 in lowercase.
- *
- * Return: 0
-*/
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int i = 0, j = 1;
+	int i;
+	int j;
 
-	while (i < 100)
+	i = 0;
+	while (i <= 99)
 	{
-	while (j < 100)
-	{
-		putchar(i / 10 + '0');
-		putchar(i % 10 + '0');
-		putchar(' ');
-		putchar(j / 10 + '0');
-		putchar(j % 10 + '0');
-		if (i != 98 || j != 99)
-	{
-		putchar(',');
-		putchar(' ');
-	}
-		j++;
-	}
-		i++;
 		j = i + 1;
+		while (j <= 99)
+		{
+			putchar(48 + i / 10);
+			putchar(48 + i % 10);
+			putchar(' ');
+			putchar(48 + j / 10);
+			putchar(48 + j % 10);
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
 	}
-		return (0);
+	putchar('\n');
+	return (0);
 }
